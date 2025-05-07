e214fc4b78d46007ef04febab6e5ab40fa2903a9
Shobhit Sharma <shobhit.sharma@i7infomedia.com>
2025-05-07 23:04:50 +0530

    rename project to capital quiz

--- capital_quiz/wsgi.py
+++ capital_quiz/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for city_quiz project.
+WSGI config for capital_quiz project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'city_quiz.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'capital_quiz.settings')
 
 application = get_wsgi_application()
