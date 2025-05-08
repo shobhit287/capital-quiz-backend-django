3963f9aa0fddb365c1ca7f952301830e80c05b8f
Shobhit Sharma <shobhit.sharma@i7infomedia.com>
2025-05-08 23:02:43 +0530

    implement cache redis

--- capital_quiz/helper.py
+++ capital_quiz/helper.py
@@ -1,10 +1,12 @@
 import requests, random
+from django.core.cache import cache
 COUNTRY_URL="https://countriesnow.space/api/v0.1/countries/capital"
 
 def get_random_country():
     response = requests.get(COUNTRY_URL)
     if response.status_code == 200:
         countries = response.json().get("data", [])
+        cache.set("all_countries", countries, timeout=60*60*24)
         random_country = random.choice(countries).get("name", None)
         return random_country
 
